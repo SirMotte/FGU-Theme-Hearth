9179963eb06bb137d2fe4a7afb5ff73734985337
Shane Parker <shane.tim.parker@gmail.com>
2021-07-03 19:28:20 -0400

    Tagged some color manager colors

--- scripts/manager_color_2.lua
+++ scripts/manager_color_2.lua
@@ -3,19 +3,19 @@
 -- attribution and copyright information.
 --
 function onInit()
-	ColorManager.COLOR_FULL = "DDDDDD";
-	ColorManager.COLOR_THREE_QUARTER = "FF6161";
-	ColorManager.COLOR_HALF = "F29476";
-	ColorManager.COLOR_QUARTER = "F5CE6C";
-	ColorManager.COLOR_EMPTY = "DDDDDD";
+	ColorManager.COLOR_FULL = "DDDDDD"; -- Replaceable Color: Usage Full
+	ColorManager.COLOR_THREE_QUARTER = "FF6161"; -- Replaceable Color: Usage Three Quarter
+	ColorManager.COLOR_HALF = "F29476"; -- Replaceable Color: Usage Half
+	ColorManager.COLOR_QUARTER = "F5CE6C"; -- Replaceable Color: Usage One Quarter
+	ColorManager.COLOR_EMPTY = "DDDDDD"; -- Replaceable Color: Usage Empty
 
 	ColorManager.COLOR_GRADIENT_TOP = { r = 0, g = 0, b = 0 };
 	ColorManager.COLOR_GRADIENT_MID = { r = 96, g = 0, b = 0 };
 	ColorManager.COLOR_GRADIENT_BOTTOM = { r = 255, g = 0, b = 0 };
 
-	ColorManager.COLOR_HEALTH_UNWOUNDED = "77CE77";
-	ColorManager.COLOR_HEALTH_DYING_OR_DEAD = "909090";
-	ColorManager.COLOR_HEALTH_UNCONSCIOUS = "C7BAEB";
+	ColorManager.COLOR_HEALTH_UNWOUNDED = "77CE77"; -- Replaceable Color: Healthy
+	ColorManager.COLOR_HEALTH_DYING_OR_DEAD = "909090"; -- Replaceable Color: Dead
+	ColorManager.COLOR_HEALTH_UNCONSCIOUS = "C7BAEB"; -- Replaceable Color: Unconscious
 
 	ColorManager.COLOR_HEALTH_SIMPLE_WOUNDED = "77CE77";
 	ColorManager.COLOR_HEALTH_SIMPLE_BLOODIED = "FF6161";
@@ -45,12 +45,12 @@ function onInit()
 	ColorManager.COLOR_TOKEN_HEALTH_GRADIENT_MID = { r = 255, g = 192, b = 0 };
 	ColorManager.COLOR_TOKEN_HEALTH_GRADIENT_BOTTOM = { r = 255, g = 0, b = 0 };
 
-	ColorManager.COLOR_ADJUSTED_HP = "D592FF";
-	ColorManager.COLOR_TEMP_HP = "69B7FF";
-	ColorManager.COLOR_PRIMARY_FOREGROUND = "EBDDBA";
-	ColorManager.COLOR_SECONDARY_FOREGROUND = "DDDDDD";
-	ColorManager.COLOR_CALENDAR_BACKGROUND = "473C33";
-	ColorManager.COLOR_CALENDAR_HOLIDAY = "FF6161";
+	ColorManager.COLOR_ADJUSTED_HP = "D592FF"; -- Replaceable Color: Adjusted HP
+	ColorManager.COLOR_TEMP_HP = "69B7FF"; -- Replaceable Color: Temporary HP
+	ColorManager.COLOR_PRIMARY_FOREGROUND = "EBDDBA";  -- Replaceable Color: Primary Foreground
+	ColorManager.COLOR_SECONDARY_FOREGROUND = "DDDDDD"; -- Replaceable Color: Secondary Foreground
+	ColorManager.COLOR_CALENDAR_BACKGROUND = "473C33"; -- Replaceable Color: Calendar Background
+	ColorManager.COLOR_CALENDAR_HOLIDAY = "FF6161"; -- Replaceable Color: Calendar Holiday
 
 	if not Bartender then
 		Interface.onHotkeyDrop = onHotkeyDrop;
