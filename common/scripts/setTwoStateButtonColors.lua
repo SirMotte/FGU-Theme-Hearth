76187387d157efa81699f300e42641344b48db18
SirMotte <82598692+SirMotte@users.noreply.github.com>
2022-07-29 00:06:06 +0200

    changes to Requested Rolls and the sidbar

--- common/scripts/setTwoStateButtonColors.lua
+++ common/scripts/setTwoStateButtonColors.lua
@@ -2,6 +2,6 @@ function onInit()
 	if super and super.onInit then
 		super.onInit()
 	end
-	setStateColor(0, ColorManager.COLOR_PRIMARY_FOREGROUND, ColorManager.COLOR_PRIMARY_FOREGROUND, ColorManager.COLOR_PRIMARY_FOREGROUND);
-	setStateColor(1, ColorManager.COLOR_SECONDARY_FOREGROUND, ColorManager.COLOR_SECONDARY_FOREGROUND, ColorManager.COLOR_SECONDARY_FOREGROUND);
+	setStateColor(0, ColorManager.COLOR_SECONDARY_FOREGROUND, ColorManager.COLOR_SECONDARY_FOREGROUND, ColorManager.COLOR_SECONDARY_FOREGROUND);
+	setStateColor(1, ColorManager.COLOR_PRIMARY_FOREGROUND, ColorManager.COLOR_PRIMARY_FOREGROUND, ColorManager.COLOR_PRIMARY_FOREGROUND);
 end
